6844e14aba3317a95e1776c34d868be5a60a818d
Apex4598 <login2ajit@gmail.com>
2022-08-13 07:44:32 +0530

    Added Fuctionality : To check Equality of line :

--- src/com/bridgelabz/LineComputation.java
+++ src/com/bridgelabz/LineComputation.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class LineComputation {
     public static double x1,x2,y1,y2,x3,y3,y4,x4,lineLength;
 
@@ -9,6 +11,13 @@ public class LineComputation {
         //System.out.println("line length :"+lineLength1);
         return lineLength;
 
+    }
+    public void checkEqual(){
+        double l1 = calculateLength(x1, y1, x2, y2);
+        double l2 = calculateLength(x3, y3, x4, y4);
+        if (Objects.equals(l1, l2)){
+            System.out.println("Lines are equal");
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comprise Program");
