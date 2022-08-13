fe4fd57ad4589f5098ef3461e36810c7a371616f
Apex4598 <login2ajit@gmail.com>
2022-08-13 07:48:25 +0530

    Added Fuctionality : To Compare two line :

--- src/com/bridgelabz/LineComputation.java
+++ src/com/bridgelabz/LineComputation.java
@@ -19,6 +19,25 @@ public class LineComputation {
             System.out.println("Lines are equal");
         }
     }
+    public void compare(){
+
+        double l1 = calculateLength(x1, y1, x2, y2);
+        double l2 = calculateLength(x3, y3, x4, y4);
+
+        //if (l1.toString().compareTo.(l2.toString())){}
+        // System.out.println((l1.toString()).compareTo(l2.toString()));
+
+        if (Double.compare(l1, l2) < 0) {
+            System.out.println("Line 1 less than line 2");
+        }
+        else if (Double.compare(l1, l2) > 0) {
+            System.out.println("Line 2 less than line 1");
+        }
+        else{
+            System.out.println("Line 1 equals to line 2");
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comprise Program");
 
